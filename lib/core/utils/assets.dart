a1a0ff1b0c19e07a4cbcf4fc489a2297ee7985d9
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-15 16:28:28 -0700

    Add some images

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -1,1 +1,11 @@
+// ignore_for_file: prefer_single_quotes
+class Assets {
+  Assets._();
+  
+  static const String imagesForget1Background = "assets/images/forget1_background.png";
+
+  static const String imagesForget2Background = "assets/images/forget2_background.png";
+
+  static const String imagesForget3Background = "assets/images/forget3_background.png";
+}
 
