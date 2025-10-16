f1bcf511ebd53ec82e98831d506bb3277362a70c
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-16 09:52:56 -0700

    add modal_progress_hud_nsn package and custom widget.

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -2,6 +2,8 @@
 class Assets {
   Assets._();
   
+  static const String animationsLoadingLottie = "assets/animations/loading_lottie.json";
+
   static const String imagesForget1Background = "assets/images/forget1_background.png";
 
   static const String imagesForget2Background = "assets/images/forget2_background.png";
---
+++ lib/core/widgets/custom_progress_hub.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:modal_progress_hud_nsn/modal_progress_hud_nsn.dart';
+
+import '../utils/assets.dart';
+
+class CustomProgressHub extends StatelessWidget {
+  const CustomProgressHub({
+    super.key,
+    required this.isLoading,
+    required this.child,
+  });
+  final bool isLoading;
+  final Widget child;
+  @override
+
+  Widget build(BuildContext context) {
+    return ModalProgressHUD(
+      progressIndicator: LoadingIconWidget(),
+      inAsyncCall: isLoading,
+      child: child,
+    );
+  }
+}
+
+class LoadingIconWidget extends StatelessWidget {
+  const LoadingIconWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 100,
+      height: 100,
+      child: Lottie.asset(Assets.animationsLoadingLottie),
+    );
+  }
+}
