406e8a2a5aafc0affe79a27eac0bd0d500c8ed1d
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-16 10:20:41 -0700

    Auth featrue ui done.

--- lib/features/auth/presentation/views/widgets/forget_password_form.dart
+++ lib/features/auth/presentation/views/widgets/forget_password_form.dart
@@ -1,9 +1,12 @@
 // ignore_for_file: use_build_context_synchronously
 
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 
 import '../../../../../core/widgets/custom_button.dart';
 import '../../../../../core/widgets/custom_phone_text_filed.dart';
+import '../verify_otp_view.dart';
 
 class ForgetPasswordForm extends StatelessWidget {
   const ForgetPasswordForm({super.key});
@@ -17,7 +20,12 @@ class ForgetPasswordForm extends StatelessWidget {
           CustomPhoneTextField(),
           SizedBox(height: 16),
           Spacer(),
-          CustomButton(onPressed: () async {}),
+          CustomButton(
+            onPressed: () {
+              log('=========');
+              Navigator.pushReplacementNamed(context, VerifyOtpView.routeName);
+            },
+          ),
         ],
       ),
     );
