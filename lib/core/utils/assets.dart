3c4f4f741c2231ee9cc3f429e30862376d70a523
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-10-16 10:51:02 -0700

    Add some images

--- lib/core/utils/assets.dart
+++ lib/core/utils/assets.dart
@@ -6,16 +6,24 @@ class Assets {
 
   static const String imagesArrowBackIcon = "assets/images/arrow_back_icon.svg";
 
-  static const String imagesBlackArrowBackIcon = "assets/images/black_arrow_back_icon.svg";
-
   static const String imagesForget1Background = "assets/images/forget1_background.png";
 
   static const String imagesForget2Background = "assets/images/forget2_background.png";
 
   static const String imagesForget3Background = "assets/images/forget3_background.png";
 
+  static const String imagesGoogleMap = "assets/images/google_map.png";
+
   static const String imagesLogo = "assets/images/logo.png";
 
+  static const String imagesNotificationsIcon = "assets/images/notifications_icon.svg";
+
+  static const String imagesSettingsIcon = "assets/images/settings_icon.svg";
+
   static const String imagesSigninBackground = "assets/images/signin_background.png";
+
+  static const String imagesTestMapImage = "assets/images/test_map_image.png";
+
+  static const String imagesTestUserImage = "assets/images/test_user_image.png";
 }
 
