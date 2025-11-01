b5a43d63e94ef64d92b9862446562cfc8583041c
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-01 12:30:42 -0700

    Pick Company Location View done.

--- lib/features/companies/presentation/views/pick_company_location_view.dart
+++ lib/features/companies/presentation/views/pick_company_location_view.dart
@@ -1,11 +1,44 @@
 import 'package:flutter/material.dart';
 
+import '../../../../core/utils/assets.dart';
+import '../../../../core/widgets/arrow_back_icon.dart';
+import '../../../../core/widgets/custom_button.dart';
+
 class PickCompanyLocationView extends StatelessWidget {
   const PickCompanyLocationView({super.key});
   static const routeName = 'PickCompanyLocation';
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Stack(
+        children: [
+          Container(
+            height: double.infinity,
+            width: double.infinity,
+            decoration: const BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(Assets.imagesTestMapImage),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Row(children: [ArrowBackIcon()]),
+              ),
+              const Spacer(),
+              CustomButton(
+                text: 'حفظ',
+                onPressed: () => Navigator.pop(context),
+              ),
+              SizedBox(height: 16),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
