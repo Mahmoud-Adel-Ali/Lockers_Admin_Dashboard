a677e69d28d0cdbd0b42e74440a7abc29130418e
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-02 12:29:25 -0800

    MaintenanceLockersView & MaintenanceUnitsView [filter location]

--- lib/features/maintenance/presentation/views/maintenance_lockers_view.dart
+++ lib/features/maintenance/presentation/views/maintenance_lockers_view.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/filter_maintenace_lockers_location_section.dart';
+
 class MaintenanceLockersView extends StatelessWidget {
   const MaintenanceLockersView({super.key});
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Maintenance Lockers')),
+    return Column(
+      children: [
+        SizedBox(height: 8),
+        FilterMaintenaceLockersLocationSection(),
+        SizedBox(height: 8),
+      ],
     );
   }
 }
--- lib/features/maintenance/presentation/views/maintenance_units_view.dart
+++ lib/features/maintenance/presentation/views/maintenance_units_view.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/filter_maintenace_units_location_section.dart';
+
 class MaintenanceUnitsView extends StatelessWidget {
   const MaintenanceUnitsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(child: Text('Maintenance Units')),
+    return Column(
+      children: [
+        SizedBox(height: 8),
+        FilterMaintenaceUnitsLocationSection(),
+        SizedBox(height: 8),
+      ],
     );
   }
 }
