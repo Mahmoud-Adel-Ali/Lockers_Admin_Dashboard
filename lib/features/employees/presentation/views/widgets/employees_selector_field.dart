0571c12ec09609be0a8fc3f39b0082ad05b0d08a
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-03 09:15:16 -0800

    Refactor EmployeesSelectorField to match last widget.

--- lib/features/employees/presentation/views/widgets/employees_selector_field.dart
+++ lib/features/employees/presentation/views/widgets/employees_selector_field.dart
@@ -1,63 +1,28 @@
 import 'package:flutter/material.dart';
 
-import '../../../../../core/utils/app_colors.dart';
-import '../../../../../core/utils/app_text_styles.dart';
+import '../../../../../core/widgets/custom_popup_menu_button.dart';
 
-class EmployeesSelectorField extends StatefulWidget {
+class EmployeesSelectorField extends StatelessWidget {
   const EmployeesSelectorField({super.key});
 
   @override
-  State<EmployeesSelectorField> createState() => _EmployeesSelectorFieldState();
-}
-
-class _EmployeesSelectorFieldState extends State<EmployeesSelectorField> {
-  // List of menu items
-  final List<String> menuItems = [
-    'كل الموظفين',
-    'إدارة شركات الشحن',
-    'إدارة الوحدات',
-    'إدارة الحجوزات',
-    'إدارة العملاء',
-  ];
-
-  // Selected value
-  String selectedItem = 'كل الموظفين';
-
-  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: PopupMenuButton<String>(
-        offset: const Offset(0, 42),
-        borderRadius: BorderRadius.circular(16),
-        // Items
-        onSelected: (value) {
-          selectedItem = value;
-          setState(() {});
-        },
-        itemBuilder: (context) => menuItems
-            .map(
-              (item) => PopupMenuItem<String>(
-                value: item,
-                child: Text(item, style: AppTextStyles.style16w400(context)),
-              ),
-            )
-            .toList(),
-        child: Container(
-          padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 10),
-          decoration: BoxDecoration(
-            color: AppColors.babyBlue,
-            borderRadius: BorderRadius.circular(16),
-          ),
-          child: Row(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              Text(selectedItem, style: AppTextStyles.style16w400(context)),
-              const SizedBox(width: 4),
-              const Icon(Icons.keyboard_arrow_down, size: 18),
-            ],
-          ),
-        ),
-      ),
+    final items = [
+      'كل الموظفين',
+      'إدارة شركات الشحن',
+      'إدارة الوحدات',
+      'إدارة الحجوزات',
+      'إدارة العملاء',
+    ];
+
+    return CustomPopupMenuButton<String>(
+      items: items,
+      initialValue: items.first,
+      itemLabelBuilder: (item) => item, // how to display each item
+      onSelected: (index) {
+        debugPrint('Selected employee filter index: $index');
+        debugPrint('Selected item: ${items[index]}');
+      },
     );
   }
 }
