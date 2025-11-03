c7e2a170dd08603955e9c627f2910ba6ce9b43d0
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-03 09:43:56 -0800

    Complaints Grid View.

--- lib/features/complaints/presentation/views/complaints_view.dart
+++ lib/features/complaints/presentation/views/complaints_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/complaints_grid_view.dart';
 import 'widgets/complaints_view_header.dart';
 
 class ComplaintsView extends StatelessWidget {
@@ -7,14 +8,16 @@ class ComplaintsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 8,
-      children: [
-        const SizedBox(),
-        const ComplaintsViewHeader(),
-        Expanded(child: Container()),
-        const SizedBox(),
-      ],
+    return Scaffold(
+      body: Column(
+        children: [
+          const SizedBox(height: 8),
+          const ComplaintsViewHeader(),
+          const SizedBox(height: 8),
+          Expanded(child: ComplaintsGridView()),
+          const SizedBox(height: 8),
+        ],
+      ),
     );
   }
 }
