4713fe163c5886e45d0dce7d1d9843ab8503bbfa
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-04 12:36:26 -0800

    add dio

--- lib/core/services/service_locator.dart
+++ lib/core/services/service_locator.dart
@@ -1,5 +1,10 @@
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
 
+// import '../api/dio_consumer.dart';
 
 final getit = GetIt.instance;
-void setupServicesLocator() {}
+void setupServicesLocator() {
+  getit.registerSingleton<Dio>(Dio());
+  // getit.registerSingleton<DioConsumer>(DioConsumer(dio: getit.get<Dio>()));
+}
