c9e7063bc232e1c7b0afed97583e50acc4770597
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-04 15:23:22 -0800

    Profile Repo

---
+++ lib/features/dashboard/data/repo/profile_repo.dart
@@ -0,0 +1,87 @@
+import 'dart:developer';
+
+import 'package:dartz/dartz.dart';
+import 'package:image_picker/image_picker.dart';
+
+import '../../../../constants.dart';
+import '../../../../core/api/dio_consumer.dart';
+import '../../../../core/api/end_points.dart';
+import '../../../../core/errors/exception.dart';
+import '../../../../core/models/location_details_model.dart';
+import '../../../../core/models/simple_model.dart';
+import '../../../../core/services/service_locator.dart';
+import '../../../auth/data/models/auth_model.dart';
+
+class ProfileRepo {
+  final DioConsumer dio = getit.get<DioConsumer>();
+
+  //* Change Password
+  Future<Either<String, SimpleModel>> changePassword({
+    required String currentPassword,
+    required String password,
+    required String passwordConfirmation,
+  }) async {
+    try {
+      var response = await dio.post(
+        EndPoints.profile,
+        data: {
+          '_method': 'put',
+          'current_password': currentPassword,
+          'password': password,
+          'password_confirmation': passwordConfirmation,
+        },
+      );
+      return Right(SimpleModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(e.errorModel.message);
+    } catch (e) {
+      return Left(kErrorMsg);
+    }
+  }
+
+  //* Update Profile
+  Future<Either<String, AuthModel>> updateProfile({
+    required XFile? image,
+    required String adminName,
+    required String name,
+    required String email,
+  }) async {
+    try {
+      var data = {
+        '_method': 'put',
+        'admin_name': adminName,
+        'name': name,
+        'email': email,
+      };
+      log("Data in updateProfile: $data");
+      final response = await dio.multipart(
+        path: EndPoints.profile,
+        imageFile: image,
+        fields: data,
+      );
+      return Right(AuthModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(e.errorModel.message);
+    } catch (e) {
+      log("Exception in updateProfile: $e");
+      return Left(kErrorMsg);
+    }
+  }
+
+  //* Update Location
+  Future<Either<String, AuthModel>> updateLocation({
+    required LocationDetailsModel locationDetailsModel,
+  }) async {
+    try {
+      var response = await dio.post(
+        EndPoints.profile,
+        data: {'_method': 'put', ...locationDetailsModel.toJson()},
+      );
+      return Right(AuthModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(e.errorModel.message);
+    } catch (e) {
+      return Left(kErrorMsg);
+    }
+  }
+}
