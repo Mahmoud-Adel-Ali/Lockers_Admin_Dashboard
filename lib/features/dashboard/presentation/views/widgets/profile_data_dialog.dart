af8297528d8f4a03d05715f8a8a5144797d1e575
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-04 15:58:33 -0800

    show admin data

--- lib/features/dashboard/presentation/views/widgets/profile_data_dialog.dart
+++ lib/features/dashboard/presentation/views/widgets/profile_data_dialog.dart
@@ -1,15 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../../core/utils/app_text_styles.dart';
-import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/size_config.dart';
 import '../../../../../core/widgets/custom_button.dart';
+import '../../../../../core/widgets/custom_cached_network_image.dart';
 import '../../../../../core/widgets/custom_dialog.dart';
 import '../../../../../core/widgets/profile_text_field.dart';
+import '../../manager/profile_provider.dart';
 import 'edit_profile_dialog.dart';
 import 'profile_password_filed.dart';
 
 Future<dynamic> profileDataDialog(BuildContext context) {
+  var prov = context.read<ProfileProvider>();
   return showDialog(
     context: context,
     builder: (context) {
@@ -27,9 +30,13 @@ Future<dynamic> profileDataDialog(BuildContext context) {
           children: [
             Text('بيانات الحساب', style: AppTextStyles.style18w400(context)),
             const SizedBox(height: 16),
-            CircleAvatar(
-              radius: 75,
-              backgroundImage: AssetImage(Assets.imagesAppLogo),
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                image: customCachedNetworkImageprovider(prov.adminData?.image),
+              ),
             ),
             const SizedBox(height: 16),
             Padding(
@@ -43,24 +50,17 @@ Future<dynamic> profileDataDialog(BuildContext context) {
                       children: [
                         ProfileTextField(
                           title: 'الإسم :',
-                          controller: TextEditingController(
-                            text: 'محمدخالد عبدالرازق',
-                          ),
-                          readOnly: true,
+                          controller: prov.nameController,
                         ),
                         ProfileTextField(
                           title: 'الإيميل :',
-                          controller: TextEditingController(
-                            text: 'www.noon.ecommerce.gov',
-                          ),
-                          readOnly: true,
+                          controller: prov.emailController,
                         ),
                         ProfileTextField(
                           title: 'رقم الهاتف :',
                           controller: TextEditingController(
-                            text: '+20 0108765434567',
+                            text: prov.adminData?.phone ?? '',
                           ),
-                          readOnly: true,
                         ),
                       ],
                     ),
