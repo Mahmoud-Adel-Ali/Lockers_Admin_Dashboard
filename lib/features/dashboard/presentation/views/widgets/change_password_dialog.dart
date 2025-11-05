664178ff67e4a6d3b2021f3a3cc62a260ba3dd04
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-04 16:06:51 -0800

    apply change password method.

--- lib/features/dashboard/presentation/views/widgets/change_password_dialog.dart
+++ lib/features/dashboard/presentation/views/widgets/change_password_dialog.dart
@@ -1,9 +1,15 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../../core/functions/snack_bar.dart';
 import '../../../../../core/widgets/custom_button.dart';
 import '../../../../../core/widgets/custom_dialog.dart';
+import '../../../../../core/widgets/custom_progress_hub.dart';
 import '../../../../../core/widgets/custom_text_password.dart';
+import '../../../../../core/widgets/dialog_helper.dart';
+import '../../manager/profile_provider.dart';
 
 Future<void> changePasswordDialog(BuildContext context) async {
   return showDialog<void>(
@@ -13,25 +19,86 @@ Future<void> changePasswordDialog(BuildContext context) async {
       return CustomDialog(
         title: 'تعديل كلمة المرور',
         message: '',
-        constraints: const BoxConstraints(maxHeight: 400, maxWidth: 500),
-        child: Column(
-          spacing: 16,
-          children: [
-            SizedBox(height: 16),
-            CustomPasswordField(hintText: 'كلمة المرور الحالية'),
-            CustomPasswordField(hintText: 'كلمة المرور الجديدة'),
-            CustomPasswordField(hintText: 'تاكيد كلمة المرور الجديدة'),
-            SizedBox(height: 16),
-            CustomButton(
-              onPressed: () {
-                Navigator.pop(context);
-                showSuccessSnackBar(context, msg: 'تم تغيير كلمة المرور بنجاح');
-              },
-              text: 'حفظ التغيرات',
-            ),
-          ],
-        ),
+        constraints: const BoxConstraints(maxHeight: 450, maxWidth: 500),
+        child: ChangePasswordForm(),
       );
     },
   );
+}
+
+class ChangePasswordForm extends StatefulWidget {
+  const ChangePasswordForm({super.key});
+
+  @override
+  State<ChangePasswordForm> createState() => _ChangePasswordFormState();
+}
+
+class _ChangePasswordFormState extends State<ChangePasswordForm> {
+  var formKey = GlobalKey<FormState>();
+  var currentPassword = TextEditingController();
+  var password = TextEditingController();
+  var passwordConfirmation = TextEditingController();
+
+  @override
+  void dispose() {
+    currentPassword.dispose();
+    password.dispose();
+    passwordConfirmation.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var prov = context.watch<ProfileProvider>();
+    return Form(
+      key: formKey,
+      child: Column(
+        spacing: 16,
+        children: [
+          SizedBox(height: 16),
+          CustomPasswordField(
+            hintText: 'كلمة المرور الحالية',
+            controller: currentPassword,
+          ),
+          CustomPasswordField(
+            hintText: 'كلمة المرور الجديدة',
+            controller: password,
+          ),
+          CustomPasswordField(
+            hintText: 'تاكيد كلمة المرور الجديدة',
+            controller: passwordConfirmation,
+          ),
+          SizedBox(height: 42),
+          (prov.checkChangingPassword == null)
+              ? LoadingIconWidget()
+              : CustomButton(
+                  onPressed: () async {
+                    if (formKey.currentState!.validate()) {
+                      formKey.currentState!.save();
+                      await prov.changePassword(
+                        currentPassword: currentPassword.text,
+                        password: password.text,
+                        passwordConfirmation: passwordConfirmation.text,
+                      );
+                      if (prov.checkChangingPassword == true) {
+                        Navigator.pop(context);
+                        showSuccessSnackBar(
+                          context,
+                          msg: prov.checkChangingPasswordMessage,
+                        );
+                      } else {
+                        DialogHelper.showErrorDialog(
+                          context,
+                          title: 'خطا',
+                          desc: prov.checkChangingPasswordMessage,
+                        );
+                      }
+                    }
+                  },
+                  text: 'حفظ التغيرات',
+                ),
+        ],
+      ),
+    );
+  }
 }
