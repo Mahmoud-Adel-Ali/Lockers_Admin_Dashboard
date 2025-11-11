2c614d8f1737b026826771e038b869fd0aed050a
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-11 14:05:46 -0800

    Create [Update Package] method in Packages Repo.

--- lib/features/packages/data/repos/packages_repo.dart
+++ lib/features/packages/data/repos/packages_repo.dart
@@ -5,7 +5,9 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/api/dio_consumer.dart';
 import '../../../../core/api/end_points.dart';
 import '../../../../core/errors/exception.dart';
+import '../../../../core/models/simple_model.dart';
 import '../../../../core/services/service_locator.dart';
+import '../models/package_model.dart';
 import '../models/packages_response_model.dart';
 
 class PackagesRepo {
@@ -20,6 +22,24 @@ class PackagesRepo {
       return Left(e.errorModel.message);
     } catch (e) {
       log("Exception in getPackages: $e");
+      return Left(e.toString());
+    }
+  }
+
+  //* Update Package
+  Future<Either<String, SimpleModel>> updatePackage({
+    required PackageModel package,
+  }) async {
+    try {
+      final response = await dio.post(
+        "${EndPoints.packages}/${package.id}",
+        data: {"_method": "put", ...package.toJson()},
+      );
+      return Right(SimpleModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(e.errorModel.message);
+    } catch (e) {
+      log("Exception in updatePackage: $e");
       return Left(e.toString());
     }
   }
