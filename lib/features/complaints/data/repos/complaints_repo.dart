31bb15edee67b832b00c1892013245954808bd87
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-11 11:37:17 -0800

    Create [Reply to complaint] method in Complaints Repo

--- lib/features/complaints/data/repos/complaints_repo.dart
+++ lib/features/complaints/data/repos/complaints_repo.dart
@@ -5,6 +5,7 @@ import 'package:dartz/dartz.dart';
 import '../../../../core/api/dio_consumer.dart';
 import '../../../../core/api/end_points.dart';
 import '../../../../core/errors/exception.dart';
+import '../../../../core/models/simple_model.dart';
 import '../../../../core/services/service_locator.dart';
 import '../models/complaints_response_model.dart';
 
@@ -25,6 +26,25 @@ class ComplaintsRepo {
       return Left(e.errorModel.message);
     } catch (error) {
       log("Exception in getAllComplaints: $error");
+      return Left(error.toString());
+    }
+  }
+
+  //* Reply to complaint
+  Future<Either<String, SimpleModel>> replyToComplaint({
+    required int id,
+    required String reply,
+  }) async {
+    try {
+      final response = await dio.post(
+        '${EndPoints.complaints}/$id',
+        data: {'_method': 'put', 'reply': reply},
+      );
+      return Right(SimpleModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(e.errorModel.message);
+    } catch (error) {
+      log("Exception in replyToComplaint: $error");
       return Left(error.toString());
     }
   }
