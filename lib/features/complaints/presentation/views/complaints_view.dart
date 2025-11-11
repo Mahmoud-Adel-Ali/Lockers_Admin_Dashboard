78cc1bf8662080c9aab834caa2c18d6fb052ce73
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-11 10:19:58 -0800

    Show Complaints part one.

--- lib/features/complaints/presentation/views/complaints_view.dart
+++ lib/features/complaints/presentation/views/complaints_view.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../../../core/widgets/api_error_widget.dart';
+import '../manager/complaints_provider.dart';
 import 'widgets/complaints_grid_view.dart';
 import 'widgets/complaints_view_header.dart';
 
@@ -8,16 +11,19 @@ class ComplaintsView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<ComplaintsProvider>();
     return Scaffold(
-      body: Column(
-        children: [
-          const SizedBox(height: 8),
-          const ComplaintsViewHeader(),
-          const SizedBox(height: 8),
-          Expanded(child: ComplaintsGridView()),
-          const SizedBox(height: 8),
-        ],
-      ),
+      body: prov.checkGettingAllComplaints == false
+          ? ApiErrorView(msg: prov.message, onRetry: prov.getAllComplaints)
+          : Column(
+              children: [
+                const SizedBox(height: 8),
+                const ComplaintsViewHeader(),
+                const SizedBox(height: 8),
+                Expanded(child: ComplaintsGridView()),
+                const SizedBox(height: 8),
+              ],
+            ),
     );
   }
 }
--- lib/features/complaints/presentation/views/widgets/complaints_grid_view.dart
+++ lib/features/complaints/presentation/views/widgets/complaints_grid_view.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/widgets/custom_user_card.dart';
+import '../../manager/complaints_provider.dart';
 import 'show_comlaints_data_dialog.dart';
 
 class ComplaintsGridView extends StatelessWidget {
@@ -9,27 +12,33 @@ class ComplaintsGridView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<ComplaintsProvider>();
+    List complaints = prov.complaints;
     return LayoutBuilder(
       builder: (context, constraints) {
         var width = constraints.maxWidth;
-        return GridView.builder(
-          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-            //* meaning that the minwidth of the card is 160.
-            crossAxisCount: (width / 160).toInt(),
-            mainAxisSpacing: 8,
-            crossAxisSpacing: 8,
-            childAspectRatio: 198.5 / 250,
-            mainAxisExtent: 190,
+        return Skeletonizer(
+          enabled: prov.checkGettingAllComplaints == null,
+          child: GridView.builder(
+            gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+              //* meaning that the minwidth of the card is 160.
+              crossAxisCount: (width / 160).toInt(),
+              mainAxisSpacing: 8,
+              crossAxisSpacing: 8,
+              childAspectRatio: 198.5 / 250,
+              mainAxisExtent: 190,
+            ),
+            itemCount: complaints.length,
+            itemBuilder: (context, index) {
+              // var complaint = complaints[index];
+              return CustomUserCard(
+                name: 'John Doe ${index + 2}',
+                phone: '1234567890',
+                assetsImage: Assets.imagesTestUserImage,
+                onTap: () => showComplaintDataDialog(context),
+              );
+            },
           ),
-          itemCount: 20,
-          itemBuilder: (context, index) {
-            return CustomUserCard(
-              name: 'John Doe ${index + 2}',
-              phone: '1234567890',
-              assetsImage: Assets.imagesTestUserImage,
-              onTap: () => showComplaintDataDialog(context),
-            );
-          },
         );
       },
     );
