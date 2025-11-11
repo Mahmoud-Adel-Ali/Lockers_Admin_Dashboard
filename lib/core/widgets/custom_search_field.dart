62b8238d0d76d06cddf4b48be1e7e1fa8ff41e65
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-11 11:33:10 -0800

    Refactor search field.

--- lib/core/widgets/custom_search_field.dart
+++ lib/core/widgets/custom_search_field.dart
@@ -3,31 +3,67 @@ import 'package:flutter/material.dart';
 import '../utils/app_colors.dart';
 import '../utils/app_text_styles.dart';
 
-class CustomSearchField extends StatelessWidget {
+class CustomSearchField extends StatefulWidget {
   const CustomSearchField({
     super.key,
     this.hintText = 'البحث',
     this.onChanged,
     this.controller,
   });
+
   final String hintText;
   final Function(String)? onChanged;
   final TextEditingController? controller;
+
+  @override
+  State<CustomSearchField> createState() => _CustomSearchFieldState();
+}
+
+class _CustomSearchFieldState extends State<CustomSearchField> {
+  late final TextEditingController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = widget.controller ?? TextEditingController();
+    _controller.addListener(() => setState(() {})); // rebuild when text changes
+  }
+
+  @override
+  void dispose() {
+    if (widget.controller == null) {
+      _controller.dispose();
+    }
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SizedBox(
       height: 42,
       child: TextField(
-        controller: TextEditingController(),
+        controller: _controller,
         textInputAction: TextInputAction.search,
-        onSubmitted: (value) {},
-        onChanged: (value) {},
         cursorColor: AppColors.main,
+        onChanged: widget.onChanged,
         style: AppTextStyles.style20w500(context).copyWith(color: Colors.black),
         decoration: InputDecoration(
-          hintText: hintText,
+          hintText: widget.hintText,
           hintStyle: AppTextStyles.style14w500(context),
           prefixIcon: const Icon(Icons.search, color: Colors.black54, size: 20),
+          suffixIcon: _controller.text.isNotEmpty
+              ? IconButton(
+                  icon: const Icon(
+                    Icons.clear,
+                    color: Colors.black54,
+                    size: 20,
+                  ),
+                  onPressed: () {
+                    _controller.clear();
+                    widget.onChanged?.call('');
+                  },
+                )
+              : null,
           contentPadding: const EdgeInsets.symmetric(horizontal: 24),
           filled: true,
           fillColor: Colors.white,
