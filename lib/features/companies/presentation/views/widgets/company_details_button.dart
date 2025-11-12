20a7f6dce36fd9d362922290333b3fb92b48fa33
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-12 14:39:37 -0800

    Update Company Status done

--- lib/features/companies/presentation/views/widgets/company_details_button.dart
+++ lib/features/companies/presentation/views/widgets/company_details_button.dart
@@ -1,9 +1,14 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../../../../../core/functions/show_loading_dialog.dart';
+import '../../../../../core/functions/snack_bar.dart';
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/widgets/custom_button.dart';
 import '../../../../../core/widgets/dialog_helper.dart';
+import '../../../../../generated/l10n.dart';
 import '../../../data/models/company_model.dart';
 import '../../manager/companies_provider.dart';
 
@@ -21,15 +26,29 @@ class CompanyDetailsButton extends StatelessWidget {
       onPressed: () {
         DialogHelper.showQuestionDialog(
           context,
-          title: 'الغاء التفعيل',
-          desc: 'هل ترغب في إيقاف تفعيل\nحساب الشركة',
+          title: !isActive ? 'تفعيل الحساب' : 'الغاء التفعيل',
+          desc: !isActive
+              ? 'هل ترغب في تفعيل\nحساب الشركة'
+              : 'هل ترغب في إيقاف تفعيل\nحساب الشركة',
           btnOkText: 'نعم',
-          onOk: () {
-            DialogHelper.showSuccessDialog(
-              context,
-              title: 'تم',
-              desc: 'تم الغاء التفعيل بنجاح',
-            );
+          onOk: () async {
+            //* Show Loading Dialog
+            showLoadingDialog(context);
+
+            await prov.updateCompanyStatus();
+
+            //* Close Loading Dialog
+            Navigator.pop(context);
+
+            if (prov.checkUpdateCompanyStatus == true) {
+              showSuccessSnackBar(context, msg: prov.message);
+            } else if (prov.checkUpdateCompanyStatus == false) {
+              DialogHelper.showErrorDialog(
+                context,
+                title: S.of(context).error,
+                desc: prov.message,
+              );
+            }
           },
         );
       },
