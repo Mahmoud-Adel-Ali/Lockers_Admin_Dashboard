5db141d1fabab824e19347eb71f8b5cb63be0540
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-13 12:55:28 -0800

    Create UnitDetailsResponse model

---
+++ lib/features/units/data/models/unit_details_response.dart
@@ -0,0 +1,29 @@
+import '../../../../core/models/unit_model.dart';
+
+class UnitDetailsResponse {
+  final bool status;
+  final String message;
+  final UnitModel? data;
+
+  UnitDetailsResponse({
+    required this.status,
+    required this.message,
+    required this.data,
+  });
+
+  factory UnitDetailsResponse.fromJson(Map<String, dynamic> json) {
+    return UnitDetailsResponse(
+      status: json['status'] ?? false,
+      message: json['message'] ?? '',
+      data: json['data'] != null ? UnitModel.fromJson(json['data']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'status': status,
+      'message': message,
+      'data': data?.toJson(),
+    };
+  }
+}
