23b43d0380a554ae45b0cf854a310dc75ef3e486
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-13 16:17:05 -0800

    Apply Sent Locker to maintenance

--- lib/features/units/presentation/views/widgets/locker_card.dart
+++ lib/features/units/presentation/views/widgets/locker_card.dart
@@ -1,14 +1,22 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:lockers_admin_dashboard/core/enum/locker_size.dart';
 import 'package:lockers_admin_dashboard/core/extensions/locker_extension.dart';
+import 'package:provider/provider.dart';
 
+import '../../../../../core/functions/check_unauthenticated.dart';
 import '../../../../../core/functions/is_arabic.dart';
+import '../../../../../core/functions/show_loading_dialog.dart';
+import '../../../../../core/functions/snack_bar.dart';
 import '../../../../../core/models/locker_model.dart';
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/app_text_styles.dart';
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/widgets/dialog_helper.dart';
+import '../../../../../generated/l10n.dart';
+import '../../manager/units_provider.dart';
 import 'edit_locker_dialog.dart';
 
 class LockerCard extends StatelessWidget {
@@ -84,44 +92,67 @@ class LockerCard extends StatelessWidget {
               ),
             ],
           ),
-          InkWell(
-            onTap: () {
-              DialogHelper.showQuestionDialog(
-                context,
-                title: 'الصيانه',
-                desc: 'هل تريد ارسال الخزنه للصيانه',
-                onOk: () {
-                  DialogHelper.showSuccessDialog(
-                    context,
-                    title: 'تم',
-                    desc: 'تم ارسال الخزنه للصيانه بنجاح',
-                  );
-                },
-              );
-            },
-            child: Container(
-              padding: const EdgeInsets.all(12.0),
-              decoration: const BoxDecoration(
-                color: AppColors.phosphorGreen,
-                borderRadius: BorderRadius.only(
-                  bottomLeft: Radius.circular(16),
-                  bottomRight: Radius.circular(16),
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Text(
-                    'إرسال إلي الصيانة',
-                    style: AppTextStyles.style14w500(
-                      context,
-                    ).copyWith(color: AppColors.white),
-                  ),
-                ],
-              ),
-            ),
-          ),
+          SendLockerButton(locker: locker),
         ],
+      ),
+    );
+  }
+}
+
+class SendLockerButton extends StatelessWidget {
+  const SendLockerButton({super.key, required this.locker});
+  final LockerModel locker;
+  @override
+  Widget build(BuildContext context) {
+    var prov = context.watch<UnitsProvider>();
+    return InkWell(
+      onTap: () {
+        DialogHelper.showQuestionDialog(
+          context,
+          title: 'الصيانه',
+          desc: 'هل تريد ارسال الخزنه للصيانه',
+          onOk: () async {
+            //* Show Loading Dialog
+            showLoadingDialog(context);
+
+            await prov.sendLockerToMantenance(id: locker.id);
+
+            ///* Close Loading Dialog
+            Navigator.pop(context);
+
+            if (prov.checkSendingLockerToMantenance == true) {
+              showSuccessSnackBar(context, msg: prov.message);
+            } else if (prov.checkSendingLockerToMantenance == false) {
+              checkUnauthenticated(context, msg: prov.message);
+              DialogHelper.showErrorDialog(
+                context,
+                title: S.of(context).error,
+                desc: prov.message,
+              );
+            }
+          },
+        );
+      },
+      child: Container(
+        padding: const EdgeInsets.all(12.0),
+        decoration: const BoxDecoration(
+          color: AppColors.phosphorGreen,
+          borderRadius: BorderRadius.only(
+            bottomLeft: Radius.circular(16),
+            bottomRight: Radius.circular(16),
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Text(
+              'إرسال إلي الصيانة',
+              style: AppTextStyles.style14w500(
+                context,
+              ).copyWith(color: AppColors.white),
+            ),
+          ],
+        ),
       ),
     );
   }
