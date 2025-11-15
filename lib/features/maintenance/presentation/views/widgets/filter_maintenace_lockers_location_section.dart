d823471ce36e136aecbfc5ae98e77459c134be3e
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-15 12:21:33 -0800

    Show [Units & Lockers] Regions

--- lib/features/maintenance/presentation/views/widgets/filter_maintenace_lockers_location_section.dart
+++ lib/features/maintenance/presentation/views/widgets/filter_maintenace_lockers_location_section.dart
@@ -1,58 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-import '../../../../../../constants.dart';
+import '../../../../../core/utils/size_config.dart';
 import '../../../../../core/widgets/location_filter_button.dart';
+import '../../manager/maintenance_provider.dart';
 
-class FilterMaintenaceLockersLocationSection extends StatefulWidget {
+class FilterMaintenaceLockersLocationSection extends StatelessWidget {
   const FilterMaintenaceLockersLocationSection({super.key});
 
   @override
-  State<FilterMaintenaceLockersLocationSection> createState() =>
-      _FilterMaintenaceLockersLocationSectionState();
-}
-
-class _FilterMaintenaceLockersLocationSectionState
-    extends State<FilterMaintenaceLockersLocationSection> {
-  int selectedCity = 0;
-  int selectedRegion = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 8,
-      children: [
-        // Cities
-        SizedBox(
-          height: 38,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: cities.length,
-            itemBuilder: (context, index) {
-              return LocationFilterButton(
-                title: cities[index],
-                isSelected: selectedCity == index,
-                onTap: () => setState(() => selectedCity = index),
-              );
-            },
-          ),
-        ),
+    var prov = context.watch<MaintenanceProvider>();
+    var cities = prov.regionsOfMaintenanceLockers;
+    var neighborhoods = cities.isEmpty
+        ? []
+        : cities[prov.selectedLockersCity].neighborhoods;
+    return cities.isEmpty
+        ? Center(child: Text('لا يوجد مدن'))
+        : Column(
+            spacing: 8,
+            children: [
+              // Cities
+              SizedBox(
+                height: 38,
+                child: Skeletonizer(
+                  enabled: prov.checkGetRegionsOfMaintenanceLockers == null,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: cities.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: LocationFilterButton(
+                          title: cities[index].city,
+                          isSelected: prov.selectedLockersCity == index,
+                          onTap: () => prov.changeLockersCity(index),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
 
-        // Regions
-        SizedBox(
-          height: 38,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: regions.length,
-            itemBuilder: (context, index) {
-              return LocationFilterButton(
-                title: regions[index],
-                isSelected: selectedRegion == index,
-                onTap: () => setState(() => selectedRegion = index),
-              );
-            },
-          ),
-        ),
-      ],
-    );
+              // Regions
+              if (neighborhoods.isNotEmpty)
+                SizedBox(
+                  height: 38,
+                  width: SizeConfig.width,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        SizedBox(width: 8),
+                        LocationFilterButton(
+                          title: 'الكل',
+                          isSelected: prov.selectedLockersRegion == -1,
+                          onTap: () => prov.changeLockersRegion(-1),
+                        ),
+                        SizedBox(width: 8),
+                        SizedBox(
+                          height: 38,
+                          child: Skeletonizer(
+                            enabled:
+                                prov.checkGetRegionsOfMaintenanceLockers ==
+                                null,
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              scrollDirection: Axis.horizontal,
+                              itemCount: neighborhoods.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 8.0,
+                                  ),
+                                  child: LocationFilterButton(
+                                    title: neighborhoods[index],
+                                    isSelected:
+                                        prov.selectedLockersRegion == index,
+                                    onTap: () =>
+                                        prov.changeLockersRegion(index),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          );
   }
 }
--- lib/features/maintenance/presentation/views/widgets/filter_maintenace_units_location_section.dart
+++ lib/features/maintenance/presentation/views/widgets/filter_maintenace_units_location_section.dart
@@ -1,58 +1,95 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
-import '../../../../../../constants.dart';
+import '../../../../../core/utils/size_config.dart';
 import '../../../../../core/widgets/location_filter_button.dart';
+import '../../manager/maintenance_provider.dart';
 
-class FilterMaintenaceUnitsLocationSection extends StatefulWidget {
+class FilterMaintenaceUnitsLocationSection extends StatelessWidget {
   const FilterMaintenaceUnitsLocationSection({super.key});
 
   @override
-  State<FilterMaintenaceUnitsLocationSection> createState() =>
-      _FilterMaintenaceUnitsLocationSectionState();
-}
-
-class _FilterMaintenaceUnitsLocationSectionState
-    extends State<FilterMaintenaceUnitsLocationSection> {
-  int selectedCity = 0;
-  int selectedRegion = 0;
-
-  @override
   Widget build(BuildContext context) {
-    return Column(
-      spacing: 8,
-      children: [
-        // Cities
-        SizedBox(
-          height: 38,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: cities.length,
-            itemBuilder: (context, index) {
-              return LocationFilterButton(
-                title: cities[index],
-                isSelected: selectedCity == index,
-                onTap: () => setState(() => selectedCity = index),
-              );
-            },
-          ),
-        ),
+    var prov = context.watch<MaintenanceProvider>();
+    var cities = prov.regionsOfMaintenanceUnits;
+    var neighborhoods = cities.isEmpty
+        ? []
+        : cities[prov.selectedUnitsCity].neighborhoods;
+    return cities.isEmpty
+        ? Center(child: Text('لا يوجد مدن'))
+        : Column(
+            spacing: 8,
+            children: [
+              // Cities
+              SizedBox(
+                height: 38,
+                child: Skeletonizer(
+                  enabled: prov.checkGetRegionsOfMaintenanceUnits == null,
+                  child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: cities.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 8.0),
+                        child: LocationFilterButton(
+                          title: cities[index].city,
+                          isSelected: prov.selectedUnitsCity == index,
+                          onTap: () => prov.changeUnitsCity(index),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ),
 
-        // Regions
-        SizedBox(
-          height: 38,
-          child: ListView.builder(
-            scrollDirection: Axis.horizontal,
-            itemCount: regions.length,
-            itemBuilder: (context, index) {
-              return LocationFilterButton(
-                title: regions[index],
-                isSelected: selectedRegion == index,
-                onTap: () => setState(() => selectedRegion = index),
-              );
-            },
-          ),
-        ),
-      ],
-    );
+              // Regions
+              if (neighborhoods.isNotEmpty)
+                SizedBox(
+                  height: 38,
+                  width: SizeConfig.width,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        SizedBox(width: 8),
+                        LocationFilterButton(
+                          title: 'الكل',
+                          isSelected: prov.selectedUnitsRegion == -1,
+                          onTap: () => prov.changeUnitsRegion(-1),
+                        ),
+                        SizedBox(width: 8),
+                        SizedBox(
+                          height: 38,
+                          child: Skeletonizer(
+                            enabled:
+                                prov.checkGetRegionsOfMaintenanceUnits == null,
+                            child: ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              scrollDirection: Axis.horizontal,
+                              itemCount: neighborhoods.length,
+                              itemBuilder: (context, index) {
+                                return Padding(
+                                  padding: EdgeInsets.symmetric(
+                                    horizontal: 8.0,
+                                  ),
+                                  child: LocationFilterButton(
+                                    title: neighborhoods[index],
+                                    isSelected:
+                                        prov.selectedUnitsRegion == index,
+                                    onTap: () => prov.changeUnitsRegion(index),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+            ],
+          );
   }
 }
