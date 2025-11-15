e2c34cb7cef3971723b9726a903efd2100864c8e
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-15 15:56:51 -0800

    Create Add Employee in Employees Repo

--- lib/features/employees/data/repos/employees_repo.dart
+++ lib/features/employees/data/repos/employees_repo.dart
@@ -1,13 +1,16 @@
 import 'dart:developer';
 
 import 'package:dartz/dartz.dart';
+import 'package:image_picker/image_picker.dart';
 
 import '../../../../constants.dart';
 import '../../../../core/api/dio_consumer.dart';
 import '../../../../core/api/end_points.dart';
 import '../../../../core/errors/exception.dart';
+import '../../../../core/models/simple_model.dart';
 import '../../../../core/services/service_locator.dart';
 import '../models/all_employees_model.dart';
+import '../models/employee_model.dart';
 
 class EmployeesRepo {
   final DioConsumer dio = getit.get<DioConsumer>();
@@ -26,6 +29,27 @@ class EmployeesRepo {
       return Left(e.errorModel.message);
     } catch (e) {
       log("Exception in getAllEmployees: $e");
+      return Left(kErrorMsg);
+    }
+  }
+
+  //* Add New Employee
+  Future<Either<String, SimpleModel>> addNewEmployee({
+    required XFile image,
+    required EmployeeModel employee,
+  }) async {
+    try {
+      log('addNewEmployee [Admin]:${employee.toJson()}');
+      final response = await dio.multipart(
+        path: EndPoints.allEmployees,
+        imageFile: image,
+        fields: employee.toJson(),
+      );
+      return Right(SimpleModel.fromJson(response));
+    } on ServerException catch (e) {
+      return Left(e.errorModel.message);
+    } catch (e) {
+      log("Exception in addNewEmployee [Admin]: $e");
       return Left(kErrorMsg);
     }
   }
