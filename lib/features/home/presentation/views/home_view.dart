5174a78e266da40e34f8e8dc7a10aaa98945da35
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-17 12:55:05 -0800

    Show Dashboard data part one

--- lib/features/home/presentation/views/home_view.dart
+++ lib/features/home/presentation/views/home_view.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../core/utils/size_config.dart';
+import '../../../../core/widgets/api_error_widget.dart';
+import '../manager/home_provider.dart';
 import 'widgets/home_view_header.dart';
 import 'widgets/lockers_indecator.dart';
 import 'widgets/sales_section.dart';
@@ -11,30 +14,33 @@ class HomeView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<HomeProvider>();
     SizeConfig.init(context);
-    return SingleChildScrollView(
-      child: Column(
-        children: [
-          const SizedBox(height: 8),
-          const HomeViewHeader(),
-          const SizedBox(height: 8),
-          // [Units & Lockers] Circle-Percent-Indicator
-          SizeConfig.isMobile()
-              ? Column(
-                  spacing: 8,
-                  children: [UnitsIndecator(), LockersIndecator()],
-                )
-              : Row(
-                  spacing: 8,
-                  children: [
-                    Expanded(child: UnitsIndecator()),
-                    Expanded(child: LockersIndecator()),
-                  ],
-                ),
-          const SizedBox(height: 8),
-          SalesSection(),
-        ],
-      ),
-    );
+    return prov.checkGetHomeDashboardData == false
+        ? ApiErrorView(msg: prov.message, onRetry: prov.getHomeDashboardData)
+        : SingleChildScrollView(
+            child: Column(
+              children: [
+                const SizedBox(height: 8),
+                const HomeViewHeader(),
+                const SizedBox(height: 8),
+                // [Units & Lockers] Circle-Percent-Indicator
+                SizeConfig.isMobile()
+                    ? Column(
+                        spacing: 8,
+                        children: [UnitsIndecator(), LockersIndecator()],
+                      )
+                    : Row(
+                        spacing: 8,
+                        children: [
+                          Expanded(child: UnitsIndecator()),
+                          Expanded(child: LockersIndecator()),
+                        ],
+                      ),
+                const SizedBox(height: 8),
+                SalesSection(),
+              ],
+            ),
+          );
   }
 }
--- lib/features/home/presentation/views/widgets/home_view_header.dart
+++ lib/features/home/presentation/views/widgets/home_view_header.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:skeletonizer/skeletonizer.dart';
 
 import '../../../../../core/utils/app_colors.dart';
 import '../../../../../core/utils/assets.dart';
 import '../../../../../core/utils/size_config.dart';
 import '../../../data/models/home_header_item_model.dart';
+import '../../manager/home_provider.dart';
 import 'home_header_item.dart';
 import 'horizontal_divider.dart';
 
@@ -12,29 +15,31 @@ class HomeViewHeader extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<HomeProvider>();
+
     SizeConfig.init(context);
 
     List<HomeHeaderItemModel> homeHeaderItems = [
       HomeHeaderItemModel(
-        title: '55',
+        title: prov.totalUnits.toString(),
         subtitle: 'عدد الوحدات',
         icon: Assets.imagesUnitIcon1,
         color: AppColors.filedGrey,
       ),
       HomeHeaderItemModel(
-        title: '12',
+        title: prov.totalLockers.toString(),
         subtitle: 'عدد الخزائن',
         icon: Assets.imagesLockerIcon1,
         color: AppColors.babyBlue,
       ),
       HomeHeaderItemModel(
-        title: '89',
+        title: prov.unitsUnderMaintenance.toString(),
         subtitle: 'عدد الوحدات في الصيانة',
         icon: Assets.imagesUnitIcon2,
         color: AppColors.green.shade50,
       ),
       HomeHeaderItemModel(
-        title: '56',
+        title: prov.lockersUnderMaintenance.toString(),
         subtitle: 'عدد الخزائن في الصيانة',
         icon: Assets.imagesLockerIcon2,
         color: AppColors.orange.shade50,
@@ -51,37 +56,43 @@ class HomeViewHeader extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
       ),
       child: !SizeConfig.isMobile()
-          ? Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                HomeHeaderItem(item: homeHeaderItems[0]),
-                HorizontalDivider(),
-                HomeHeaderItem(item: homeHeaderItems[1]),
-                HorizontalDivider(),
-                HomeHeaderItem(item: homeHeaderItems[2]),
-                HorizontalDivider(),
-                HomeHeaderItem(item: homeHeaderItems[3]),
-              ],
+          ? Skeletonizer(
+              enabled: prov.checkGetHomeDashboardData == null,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  HomeHeaderItem(item: homeHeaderItems[0]),
+                  HorizontalDivider(),
+                  HomeHeaderItem(item: homeHeaderItems[1]),
+                  HorizontalDivider(),
+                  HomeHeaderItem(item: homeHeaderItems[2]),
+                  HorizontalDivider(),
+                  HomeHeaderItem(item: homeHeaderItems[3]),
+                ],
+              ),
             )
-          : Column(
-              children: [
-                Row(
-                  children: [
-                    HomeHeaderItem(item: homeHeaderItems[0]),
-                    HorizontalDivider(),
-                    HomeHeaderItem(item: homeHeaderItems[1]),
-                  ],
-                ),
-                const Divider(height: 0),
-                Row(
-                  children: [
-                    HomeHeaderItem(item: homeHeaderItems[2]),
-                    HorizontalDivider(),
-                    HomeHeaderItem(item: homeHeaderItems[3]),
-                  ],
-                ),
-              ],
+          : Skeletonizer(
+              enabled: prov.checkGetHomeDashboardData == null,
+              child: Column(
+                children: [
+                  Row(
+                    children: [
+                      HomeHeaderItem(item: homeHeaderItems[0]),
+                      HorizontalDivider(),
+                      HomeHeaderItem(item: homeHeaderItems[1]),
+                    ],
+                  ),
+                  const Divider(height: 0),
+                  Row(
+                    children: [
+                      HomeHeaderItem(item: homeHeaderItems[2]),
+                      HorizontalDivider(),
+                      HomeHeaderItem(item: homeHeaderItems[3]),
+                    ],
+                  ),
+                ],
+              ),
             ),
     );
   }
