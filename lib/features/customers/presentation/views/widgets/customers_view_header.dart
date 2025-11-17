e280cffee406343f8f39455655c534681efe36ee
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-17 05:57:11 -0800

    Apply Search

--- lib/features/customers/presentation/views/widgets/customers_view_header.dart
+++ lib/features/customers/presentation/views/widgets/customers_view_header.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../../../../../core/utils/size_config.dart';
 import '../../../../../core/widgets/custom_search_field.dart';
+import '../../manager/customers_provider.dart';
 import 'customers_selector_field.dart';
 
 class CustomersViewHeader extends StatelessWidget {
@@ -13,10 +15,27 @@ class CustomersViewHeader extends StatelessWidget {
     bool isMobile = SizeConfig.isMobile();
     return Row(
       children: [
-        Expanded(child: CustomSearchField(hintText: 'ابحث عن عميل')),
+        Expanded(child: CustomersSearchField()),
         if (!isMobile) const Spacer(flex: 2) else const SizedBox(width: 16),
         CustomersSelectorField(),
       ],
+    );
+  }
+}
+
+class CustomersSearchField extends StatelessWidget {
+  const CustomersSearchField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var prov = context.watch<CustomersProvider>();
+    return CustomSearchField(
+      hintText: 'ابحث عن عميل',
+      controller: prov.searchController,
+      onChanged: (val) {
+        if (prov.checkGetAllCustomers == null) return;
+        prov.getAllCustomers();
+      },
     );
   }
 }
