ad43603236fb573777ebf752a68a3ac7aa953e67
Mahmoud Adel Ali <mahmoudadela413@gmail.com>
2025-11-17 12:56:50 -0800

    Show Dashboard data part two

--- lib/features/home/presentation/views/widgets/lockers_indecator.dart
+++ lib/features/home/presentation/views/widgets/lockers_indecator.dart
@@ -1,6 +1,7 @@
-
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../manager/home_provider.dart';
 import 'units_or_lockers_percent_indicator.dart';
 
 class LockersIndecator extends StatelessWidget {
@@ -8,12 +9,16 @@ class LockersIndecator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<HomeProvider>();
+    double percentIndecator =
+        prov.lockersUnderMaintenance /
+        (prov.totalLockers == 0 ? 1 : prov.totalLockers);
     return UnitsOrLockersPercentIndicator(
       title: 'الخزائن',
       textOne: 'الخزائن المتاحة',
       textTwo: 'خزائن في الصيانة',
-      percentIndecator: 0.15,
-      textIndecator: '29\nفي الصيانة',
+      percentIndecator: 1 - percentIndecator,
+      textIndecator: '${prov.lockersUnderMaintenance}\nفي الصيانة',
     );
   }
 }
--- lib/features/home/presentation/views/widgets/units_indecator.dart
+++ lib/features/home/presentation/views/widgets/units_indecator.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
+import '../../manager/home_provider.dart';
 import 'units_or_lockers_percent_indicator.dart';
 
 class UnitsIndecator extends StatelessWidget {
@@ -7,12 +9,16 @@ class UnitsIndecator extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    var prov = context.watch<HomeProvider>();
+    double percentIndecator =
+        prov.unitsUnderMaintenance /
+        (prov.totalUnits == 0 ? 1 : prov.totalUnits);
     return UnitsOrLockersPercentIndicator(
       title: 'الوحدات',
       textOne: 'الوحدات المتاحة',
       textTwo: 'وحدات في الصيانة',
-      percentIndecator: 0.55,
-      textIndecator: '23\nفي الصيانة',
+      percentIndecator: 1 - percentIndecator,
+      textIndecator: '${prov.unitsUnderMaintenance}\nفي الصيانة',
     );
   }
 }
